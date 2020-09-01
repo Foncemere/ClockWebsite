5d7bf81bc521dd25fdc0134a25097907d2f8752c
Karl Taguibao <karltag2018@gmail.com>
2020-09-01 16:28:07 +0800

    my first website

---
+++ stuff.js
@@ -0,0 +1,26 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.getElementById('hour-data')
+const minuteHand = document.getElementById('minute-data')
+const secondHand = document.getElementById('second-data')
+
+
+function setClock(){
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds()/60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes())/60
+    const hoursRatio = (minutesRatio + currentDate.getHours())/12
+    
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio){
+    element.style.setProperty('--rotation', rotationRatio*360)
+}
+
+console.log(secondHand);
+console.log(minuteHand);
+console.log(hourHand);
+setClock()
