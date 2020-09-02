12118f16bdde10afa7a8a4b16d4e5c5dd6eace9f
Karl Taguibao <karltag2018@gmail.com>
2020-09-02 17:56:44 +0800

    v2: night cycle

--- stuff.js
+++ stuff.js
@@ -3,24 +3,38 @@ setInterval(setClock, 1000)
 const hourHand = document.getElementById('hour-data')
 const minuteHand = document.getElementById('minute-data')
 const secondHand = document.getElementById('second-data')
-
+const clock = document.querySelector(".clock")
+const clockNumbers = document.querySelectorAll(".clock .numbers")
 
 function setClock(){
     const currentDate = new Date()
     const secondsRatio = currentDate.getSeconds()/60
     const minutesRatio = (secondsRatio + currentDate.getMinutes())/60
     const hoursRatio = (minutesRatio + currentDate.getHours())/12
-    
-    setRotation(secondHand, secondsRatio)
-    setRotation(minuteHand, minutesRatio)
-    setRotation(hourHand, hoursRatio)
+    setRotationSeconds(secondHand, secondsRatio)
+    setRotationMinutes(minuteHand, minutesRatio)
+    setRotationHours(hourHand, hoursRatio)
+
+    if (currentDate.getHours() > 14) {
+        document.body.style.background = 'linear-gradient(to top left, #301e5d, #000033)';
+        clock.style.background = 'black';
+        clock.style.border = 'solid black 17px';
+        secondHand.style.border = '1px solid black'
+        for (i = 0; i <  clockNumbers.length; i++){
+            clockNumbers[i].style.color = 'white';
+        }
+    }
 }
 
-function setRotation(element, rotationRatio){
-    element.style.setProperty('--rotation', rotationRatio*360)
+function setRotationSeconds(element, rotationRatio){
+    element.style.transform = "translateX(-50%) rotate(" + rotationRatio*360 + "deg)"
+    element.style.transition = "3s linear"
+}
+function setRotationMinutes(element, rotationRatio){
+    element.style.transform = "translateX(-50%) rotate(" + rotationRatio*360 + "deg)"
+}
+function setRotationHours(element, rotationRatio){
+    element.style.transform = "translateX(-50%) rotate(" + rotationRatio*360 + "deg)"
 }
 
-console.log(secondHand);
-console.log(minuteHand);
-console.log(hourHand);
 setClock()
